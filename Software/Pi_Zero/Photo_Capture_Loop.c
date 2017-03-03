d068ffe0d4b68a0215c99dffb7a5df57ae0876ba
cesty <cesty8@gmail.com>
2017-03-02 19:31:07 -0500

    Update Photo_Capture_Loop.c

--- Software/Pi_Zero/Photo_Capture_Loop.c
+++ Software/Pi_Zero/Photo_Capture_Loop.c
@@ -9,20 +9,28 @@
 #include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <raspicam/raspicam.h>
+using namespacestd;
 
 #include "Pulse_Functions.h"    // Calls pulse functions
 
 // #define PulsePin     0      // Pin that pulse is going too - BCM GPIO 17
 
-int main(void) {
+int main(int argc,char **argv) {
     // Write a loop that turns on the photo after every 30 shots and then turns it off after the first of the next one
     Setup_Pulse();  // initial raspberry pi for sync pulse
     
     int counter = 0;    // photo counter
-    
+    raspicam::RaspiCam Camera; //Cmaera object
+	
     for (;;) {
-	usleep(3000);
-        // Take photo
+	//capture
+    	Camera.grab();
+    	//allocate memory
+    	unsigned char *data=new unsigned char[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
         counter ++;
         if (counter == 1) {
             End_Pulse();
