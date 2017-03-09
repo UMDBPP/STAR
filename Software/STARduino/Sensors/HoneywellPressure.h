461942c217cfddc901c14651073643f2d7da42db
Nicholas Rossomando <nmrossomando@gmail.com>
2017-03-09 15:27:35 -0500

    Fixed types; added supersampling provision in pressure sensor header.

--- Software/STARduino/Sensors/HoneywellPressure.h
+++ Software/STARduino/Sensors/HoneywellPressure.h
@@ -11,13 +11,20 @@ class HoneywellPressureI2C {
         ~HoneywellPressureI2C();
 
         void recieve_data();
+
+        // Setter: numSamples
+        void set_number_samples(uint8_t _samples) { numSamples = _samples; }
+
+        // Getters for all stored values
         uint16_t get_pressure() { return pressureCounts; }
         uint16_t get_temperature() { return tempCounts; }
+        uint8_t get_number_samples() { return numSamples; }
         PRESSURE_SENSOR_STATUS get_status() { return status; }
 
     private:
-        int pressureCounts;
-        int tempCounts;
+        uint16_t pressureCounts;
+        uint16_t tempCounts;
+        uint8_t numSamples;
         PRESSURE_SENSOR_STATUS status;
         
 };
