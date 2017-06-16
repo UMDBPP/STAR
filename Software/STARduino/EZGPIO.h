c6eada426e660bd8220a68b6d8b5d459fcbaa2b3
Nicholas Rossomando <nmrossomando@gmail.com>
2017-06-16 12:27:48 -0400

    Added EZGPIO helper functions.

--- Software/STARduino/EZGPIO.h
+++ Software/STARduino/EZGPIO.h
@@ -1,4 +1,10 @@
-#define GPIO_BASE_ADDR *(unsigned int volatile *)0x41004400
+#define GPIO_BASE_ADDR 0x41004400
+#define PA 0x00
+#define PB 0x80
+#define IN 0x1
+#define OUT 0x0
+#define HI 0x1
+#define LO 0x0
 
 #define DIR_A *(unsigned int volatile *)(GPIO_BASE_ADDR)
 #define DIRCLR_A *(unsigned int volatile *)(GPIO_BASE_ADDR + 0x04)
@@ -28,10 +34,10 @@
 // By Camden Miller
 inline void set_PMUX_A(unsigned char pinnum, unsigned char value) {
     value &= 0x08;
-    unsigned char volatile * pmux_n = PMUX_A(pinnum);
+    unsigned char pmux_n = PMUX_A(pinnum);
     unsigned char offset = pinnum % 2;
 
-    unsigned char buf = *pmux_n;
+    unsigned char buf = pmux_n;
     buf &= (0xF0 >> (offset * 4));
     buf |= (value << (offset * 4));
     pmux_n = buf;
@@ -39,11 +45,85 @@ inline void set_PMUX_A(unsigned char pinnum, unsigned char value) {
 
 inline void set_PMUX_B(unsigned char pinnum, unsigned char value) {
     value &= 0x08;
-    unsigned char volatile * pmux_n = PMUX_B(pinnum);
+    unsigned char pmux_n = PMUX_B(pinnum);
     unsigned char offset = pinnum % 2;
 
-    unsigned char buf = *pmux_n;
+    unsigned char buf = pmux_n;
     buf &= (0xF0 >> (offset * 4));
     buf |= (value << (offset * 4));
     pmux_n = buf;
+}
+
+inline void set_DIR(int port, int pin, bool direction) {
+    if((pin > 31) || (pin < 0)) { return; }
+
+    if(port == PA) {
+        if(direction == IN) {
+            DIRCLR_A |= (1 << pin);
+        } else {
+            DIRSET_A |= (1 << pin);
+        }
+    } else if(port == PB) {
+        if(direction == IN) {
+            DIRCLR_B |= (1 << pin);
+        } else {
+            DIRSET_B |= (1 << pin);
+        }
+    } else {
+        return;
+    }
+}
+
+inline void toggle_DIR(int port, int pin) {
+    if((pin > 31) || (pin < 0)) { return; }
+
+    if(port == PA) {
+        DIRTGL_A |= (1 << pin);
+    } else if(port == PB) {
+        DIRTGL_B |= (1 << pin);
+    }
+}
+
+inline void set_OUTPUT(int port, int pin, bool setting) {
+    if((pin > 31) || (pin < 0)) { return; }
+
+    if(port == PA) {
+        if(setting == HI) {
+            DIRSET_A |= (1 << pin);
+        } else {
+            DIRCLR_A |= (1 << pin);
+        }
+    } else if(port == PB) {
+        if(setting == HI) {
+            DIRSET_B |= (1 << pin);
+        } else {
+            DIRCLR_B |= (1 << pin);
+        }
+    }
+}
+
+inline void toggle_OUTPUT(int port, int pin) {
+    if((pin > 31) || (pin < 0)) { return; }
+
+    if(port == PA) {
+        OUTTGL_A |= (1 << pin);
+    } else if(port == PB) {
+        OUTTGL_B |= (1 << pin);
+    }
+}
+
+inline int read_INPUT(int port, int pin) {
+    unsigned int buf;
+
+    if(port == PA) {
+        buf = IN_A;
+    } else if(port == PB) {
+        buf = IN_B;
+    }
+
+    if(buf & (1 << pin)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
--- Software/STARduino/STARduino.ino
+++ Software/STARduino/STARduino.ino
@@ -1,8 +1,4 @@
-#define BASE_ADDR *(unsigned int volatile *)0x41004400
-#define DIRSET_A *(unsigned int volatile *)(BASE_ADDR + 0x08)
-#define OUTSET_A *(unsigned int volatile *)(BASE_ADDR + 0x18)
-
-#define RESET_REGISTER *(unsigned int volatile *)0xE000ED0C
+#include "EZGPIO.h"
 
 /* Define Functions */
 void setup() {
@@ -20,6 +16,8 @@ void setup() {
      none
 
   */
+  Serial.begin(9600);
+  
   // Pull "Kill" ports high:
   pinMode(11, OUTPUT); // Pi Kill
   digitalWrite(11, HIGH);
@@ -27,14 +25,12 @@ void setup() {
   //digitalWrite(38, HIGH);
   DIRSET_A |= (1 << 13);
   OUTSET_A |= (1 << 13);
-  delay(5000);
-  digitalWrite(11,LOW);
-  delay(5000);
 
 }
 
 void loop() {
   //Serial.println("TEST.");
+
   delay(1000);
 }
 
