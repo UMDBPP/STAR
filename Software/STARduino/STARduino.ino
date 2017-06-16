a26b28d7d196b390505f08c3f79f5dbf5e027e63
Nicholas Rossomando <nmrossomando@gmail.com>
2017-06-16 17:41:43 -0400

    Update test sketch. EZGPIO functions not working; bare registers are.

--- Software/STARduino/STARduino.ino
+++ Software/STARduino/STARduino.ino
@@ -1,5 +1,10 @@
 #include "EZGPIO.h"
 
+int LEDPIN = 9;
+int BIASPIN = 28;
+int PIKILL = 16;
+int EXTKILL = 13;
+
 /* Define Functions */
 void setup() {
   /*
@@ -19,26 +24,23 @@ void setup() {
   Serial.begin(9600);
   
   // Pull "Kill" ports high:
-  pinMode(11, OUTPUT); // Pi Kill
-  digitalWrite(11, HIGH);
-  //pinMode(38, OUTPUT); // External Kill
-  //digitalWrite(38, HIGH);
-  //set_DIR(PA, 13, false);
-  //set_OUTPUT(PA, 13, true);
-  //set_DIR(PA, 28, false);
-  DIRSET_A = (1 << 13);
-  OUTSET_A = (1 << 13);
-  DIRSET_A = (1 << 28);
+  DIRSET_A = (1 << PIKILL);
+  OUTSET_A = (1 << PIKILL);
+  DIRSET_A = (1 << EXTKILL);
+  OUTSET_A = (1 << EXTKILL);
+  
+  DIRSET_A = (1 << BIASPIN);
+  DIRSET_A = (1 << LEDPIN);
   
 }
 
 void loop() {
   //Serial.println("TEST.");
-  //set_OUTPUT(PA, 28, true);
-  OUTTGL_A = (1 << 28);
+  //OUTTGL_A = (1 << BIASPIN);
+  OUTTGL_A = (1 << LEDPIN);
   delay(5000);
-  //set_OUTPUT(PA, 28, false);
-  OUTTGL_A = (1 << 28);
+  OUTTGL_A = (1 << BIASPIN);
+  //OUTTGL_A = (1 << LEDPIN);
   delay(5000);
 }
 
